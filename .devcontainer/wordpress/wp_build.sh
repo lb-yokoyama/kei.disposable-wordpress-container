29a1c9ef95a9fb2ae46fe677fe3357dce39dba50
nominalrune <59037577+nominalrune@users.noreply.github.com>
2021-12-14 12:59:04 +0900

    Update wp_build.sh

--- .devcontainer/wordpress/wp_build.sh
+++ .devcontainer/wordpress/wp_build.sh
@@ -2,16 +2,15 @@
 mv /usr/tmpl/nginx.repo /etc/yum.repos.d/nginx.repo
 echo "hi, this is wp_build v0.1.2"
 exec /sbin/init &
-yum update -y
-yum install -y https://rpms.remirepo.net/enterprise/remi-release-7.rpm
-yum install -y --enablerepo=remi-php74 php-fpm php-mbstring php-pdo php-gd php-mysqlnd nginx yum-cron git vsftpd
-systemctl enable nginx
-systemctl enable php-fpm
-systemctl enable yum-cron
-systemctl enable vsftpd
-echo "NGINX php-fpm starts"
-nginx && php-fpm -D
-echo "NGINX started, it worked."
-mv -f /usr/tmpl/wp.conf /etc/nginx/conf.d/default.conf
+yum update -y \
+  && yum install -y https://rpms.remirepo.net/enterprise/remi-release-7.rpm \
+  && yum install -y --enablerepo=remi-php74 \
+       php-fpm php-mbstring php-pdo php-gd php-mysqlnd \
+       nginx yum-cron git vsftpd
+systemctl enable nginx && systemctl enable php-fpm && systemctl enable yum-cron && systemctl enable vsftpd
+echo "NGINX php-fpm starts" \
+  && nginx && php-fpm -D \
+  && echo "NGINX started, it worked."
+mv -f /usr/tmpl/wp.conf /etc/nginx/conf.d/default.conf \
+ && mv -f /usr/tmpl/php.www.conf /etc/php-fpm.d/www.conf
 sed 's/apply updates = no/apply updates = yes/' /etc/yum/yum-cron.conf>/etc/yum/yum-cron.conf
-mv -f /usr/tmpl/php.www.conf /etc/php-fpm.d/www.conf
